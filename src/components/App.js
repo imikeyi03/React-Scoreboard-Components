11fcd8aab1eb0c95be8af760a70fc90c8c918d46
mikey mann <mikey.mannx3@gmail.com>
2021-03-04 15:50:25 -0800

    wokring on unidirectional flow

--- src/components/App.js
+++ src/components/App.js
@@ -9,22 +9,57 @@ class App extends React.Component {
     players: [
       {
         name: "Mikey",
+        score: 0,
         id: 1
       },
       {
         name: "Lily",
+        score: 0,
         id: 2
       },
       {
         name: "Taylor",
+        score: 0,
         id: 3
       },
       {
         name: "Kato",
+        score: 0,
         id: 4
       }
     ]
   };
+
+
+  handleScoreChange = (index, delta) => {
+    this.setState( prevState => ({
+      score: prevState.players[index].score += delta
+    }));
+    console.log('index: ' + index, 'delta: ' + delta);
+  }
+
+  
+  // Below is a way to update the score change without directly mutating the the original state
+  
+  // handleScoreChange = (index, delta) => {
+  //   this.setState( prevState => {
+  //     // New 'players' array – a copy of the previous `players` state
+  //     const updatedPlayers = [ ...prevState.players ];
+  //     // A copy of the player object we're targeting
+  //     const updatedPlayer = { ...updatedPlayers[index] };
+
+  //     // Update the target player's score
+  //     updatedPlayer.score += delta;
+  //     // Update the 'players' array with the target player's latest score
+  //     updatedPlayers[index] = updatedPlayer;
+
+  //     // Update the `players` state without mutating the original state
+  //     return {
+  //       players: updatedPlayers
+  //     };
+  //   });
+  // }
+
 
   handleRemovePlayer = (id) => {
     this.setState( prevState => {
@@ -43,11 +78,14 @@ class App extends React.Component {
         />
   
         {/* Players list */}
-        {this.state.players.map( player =>
+        {this.state.players.map( (player, index) =>
           <Player 
             name={player.name}
+            score={player.score}
             id={player.id}
-            key={player.id.toString()} 
+            key={player.id.toString()}
+            index={index}
+            changeScore={this.handleScoreChange} 
             removePlayer={this.handleRemovePlayer}           
           />
         )}
