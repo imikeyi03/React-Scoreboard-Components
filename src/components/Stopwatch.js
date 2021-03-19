17b87777f341c0ebc3a99cf073355a40fb1775dc
Mikey Mann <mikey.mannx3@gmail.com>
2021-03-19 13:58:48 -0700

    Added stopwatch.js

---
+++ src/components/Stopwatch.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Stopwatch extends Component {
+    
+    state = {
+        isRunning: false
+    };
+    
+    
+    tick = () => {
+        this.componentDidMount()
+    };
+    
+    
+    handleStopwatch = () => {
+        this.setState({
+            isRunning: !this.state.isRunning
+        });
+    };
+
+    
+    
+    
+    render() {
+        return(
+            <div className="stopwatch">
+                <h2>Stopwatch</h2>
+                <span className="stopwatch-time">0</span>
+                <button onClick={this.handleStopwatch}>
+                    {this.state.isRunning ? 'Stop' : 'Start' }
+                </button>
+                <button>Reset</button>
+            </div>
+        );
+    
+    }
+}
+
+export default Stopwatch;
